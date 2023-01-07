71b7f6e8acbcbbfcd161e71d56cfc92e9a4fe52f
daniel bernardo castro <dandan682@outlook.com>
2023-01-07 00:13:19 -0600

    update java

--- java/EjemploCaracteres.java
+++ java/EjemploCaracteres.java
@@ -2,6 +2,10 @@ public class EjemploCaracteres {
     public static void main(String[] args) {
         char caracter = 'a';
         System.out.println(caracter);
+        caracter = 65;
+        System.out.println(caracter);
+        caracter = 65 + 1;
+        System.out.println(caracter);
         
     }
 }
