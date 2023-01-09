62213687c08c09f5714a7b2fcf73ab8af5e3d352
daniel bernardo castro <dandan682@outlook.com>
2023-01-08 23:18:01 -0600

    update java

--- java/EjemploCaracteres.java
+++ java/EjemploCaracteres.java
@@ -5,6 +5,10 @@ public class EjemploCaracteres {
         caracter = 65;
         System.out.println(caracter);
         caracter = 65 + 1;
-        System.out.println(caracter);
+        System.out.println(caracter + 1);
+        String palabra = "Alura";
+        System.out.println(palabra);
+        palabra = palabra + " 2023";
+        System.out.println(palabra);
     }
 }
